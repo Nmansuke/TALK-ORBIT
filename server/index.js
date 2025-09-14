e04a7dd10dfc14d00e15e922fff64fa32fa7751e
Nmansuke <mansukenagesh@gmail.com>
2025-09-14 23:19:54 +0530

    Update environment, server setup, and package configuration

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import contactsRoutes from "./routes/ContactRoutes.js";
 import messagesRoutes from "./routes/MessagesRoute.js";
 import setupSocket from "./socket.js";
 import channelRoutes from "./routes/ChannelRoutes.js";
+import path from 'path';
 
 dotenv.config();
 
@@ -33,6 +34,18 @@ app.use("/api/auth", authRoutes);
 app.use("/api/contacts", contactsRoutes);
 app.use("/api/messages", messagesRoutes);
 app.use("/api/channel", channelRoutes);
+
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV === 'production'){
+app.use(express.static(path.join(__dirname1,'/client/build')));
+app.get('*', (req,res)=>{
+  res.sendFile(path.resolve(__dirname1,"client","dist", "index.html"));
+});
+} else{
+  app.get("/",(req,res)=>{
+    res.send("api is runing");
+  });
+}
 
 const server = app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
